753d7c11f776e87ee15dcda5786a131a2b5c6fc5
Daniel J Quest;Quest.Daniel@mayo.edu;15747891 <m102417@franklin03.mayo.edu>
2016-08-25 15:37:04 -0500

    Removing unwanted file

--- test_toolkit.py
+++
@@ -1,8 +0,0 @@
-from unittest import TestCase
-
-import microbe_toolbox
-
-class TestJoke(TestCase):
-    def test_is_string(self):
-        s = funniest.joke()
-        self.assertTrue(isinstance(s, basestring))
