ab51f091e27a648582c6186c008051a9cb16d2e7
M102417 <quest.daniel@mayo.edu>
2016-08-25 14:27:14 -0500

    Starting to build out our package management system...

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='microbe_toolbox',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='http://github.com/storborg/funniest',
+      author='Flying Circus',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['microbe_toolbox'],
+      zip_safe=False)
